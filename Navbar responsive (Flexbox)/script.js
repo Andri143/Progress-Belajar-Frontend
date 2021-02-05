9a4287ac3c4d8976047e5d82c2ef2c3e9b700796
Andri febrianto <77367722+Andri143@users.noreply.github.com>
2021-02-05 20:07:21 +0700

    Bikin navbar CSS + Javascript sederhana

---
+++ Navbar responsive (Flexbox)/script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('nav .menu-toggle');
+
+const menuList = document.querySelector('nav ul');	
+
+menuToggle.addEventListener('click', function(){
+
+	menuList.classList.toggle('slide');
+
+});
